39c856138457a13c8a766be5dc761a8babb2ccd6
lareenkado <lareenkado@gmail.com>
2025-11-30 11:57:00 +0200

    Update main_server.py

--- main_server.py
+++ main_server.py
@@ -1,16 +1,20 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
+@app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("page_home.html")
+
 @app.route("/auth")
 def auth():
-    return render_template("auth.html")
+    return render_template("page_auth.html")
+
 @app.route("/checkin")
 def checkin():
-    return render_template("checkin.html")
+    return render_template("page_checkin.html")
+
 @app.route("/dashboard")
 def dashboard():
-    return render_template("dashboard.html")
+    return render_template("page_dashboard.html")
 if __name__ == "__main__":
     app.run(debug=True)
